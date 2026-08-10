0ed64e1a112e584018e84c8e12041a107e970fc5
Sophia LaSala <sophia.lasala@outlook.com>
2026-08-10 19:17:44 -0400

    Create frequency_response.py

---
+++ Frequency_Response/frequency_response.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+
+print("Everything works")
+print("NumPy:", np.__version__)
+print("pandas:", pd.__version__)
+print("SciPy:", scipy.__version__)
